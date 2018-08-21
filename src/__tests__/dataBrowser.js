8027b208f487773e4d58e2f8c366b7d5533e00fe
davidalekna <alekna@live.co.uk>
2018-08-21 19:50:25 +0100

    test updates

--- src/__tests__/dataBrowser.js
+++ src/__tests__/dataBrowser.js
@@ -1,14 +1,31 @@
+import 'react-testing-library/cleanup-after-each';
 import React from 'react';
-import ReactDom from 'react-dom';
-import DataBrowser from '../index';
+import { render, fireEvent } from 'react-testing-library';
+import DataBrowser from '../';
 
 test('mounts', () => {
-  const div = document.createElement('div');
-  ReactDom.render(
-    <DataBrowser data={[]} columns={[]}>
-      {() => <div>asd</div>}
-    </DataBrowser>,
-    div,
-  );
-  console.log(div.innerHTML);
+  const { selectItem } = setup({
+    data: [],
+    columns: [],
+  });
+
+  // const div = document.createElement('div');
+  // ReactDom.render(
+  //   <DataBrowser data={[]} columns={[]}>
+  //     {() => <div>asd</div>}
+  //   </DataBrowser>,
+  //   div,
+  // );
+
+  // console.log(div.innerHTML);
 });
+
+function setup({ render: renderFn = () => <div />, ...props } = {}) {
+  let renderArg;
+  const childrenSpy = jest.fn(controllerArg => {
+    renderArg = controllerArg;
+    return renderFn(controllerArg);
+  });
+  const utils = render(<DataBrowser {...props}>{childrenSpy}</DataBrowser>);
+  return { childrenSpy, ...utils, ...renderArg };
+}
