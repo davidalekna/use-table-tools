9a4c12d2a5375b4c05bd792991e47fd184d337e5
davidalekna <alekna@live.co.uk>
2018-08-24 13:45:04 +0100

    checkboxState should check if checbox on or off test

--- src/__tests__/dataBrowser.js
+++ src/__tests__/dataBrowser.js
@@ -6,29 +6,30 @@ import DataBrowser from '../';
 import mockColumns from '../__mocks__/columns';
 import mockData from '../__mocks__/data';
 
-// selected,
-// data,
-// viewType,
-// switchViewType,
-// viewsAvailable,
-// selectAllCheckboxState,
-// currentSort,
-// checked,
-// switchColumns,
-// checkboxState,
-// offsetColumns,
-// checkboxToggle,
-// onSelection,
-// changeSortDirection,
-// defaultSortMethod,
-// sortData,
-// activeSort,
-
 test('should return visibleColumns in shape of columnFlex', () => {
   const { columnFlex, visibleColumns } = setup({
     columnFlex: ['0 0 25%', '1 1 55%', '0 0 20%'],
   });
   expect(visibleColumns.length).toEqual(columnFlex.length);
+});
+
+test('switchViewType should switch to selected available view', () => {
+  const handleStateChange = jest.fn();
+  const { switchViewType } = setup({
+    onStateChange: handleStateChange,
+    viewsAvailable: ['LIST_VIEW', 'GRID_VIEW'],
+  });
+  const viewType = { viewType: 'GRID_VIEW' };
+  switchViewType(viewType);
+  const changes = {
+    type: '__switch_view__',
+    ...viewType,
+  };
+  expect(handleStateChange).toHaveBeenCalledTimes(1);
+  expect(handleStateChange).toHaveBeenLastCalledWith(
+    changes,
+    expect.objectContaining({ viewType: changes.viewType }),
+  );
 });
 
 test('switchColumns should switch column accordingly', () => {
@@ -49,6 +50,24 @@ test('switchColumns should switch column accordingly', () => {
     changes,
     expect.objectContaining({ visibleColumns: changes.visibleColumns }),
   );
+});
+
+test('checkboxState should check if checbox on or off', () => {
+  const { checkboxState } = setup({
+    checked: [1, 2, 3, 4],
+  });
+  const result_1 = checkboxState(2);
+  expect(result_1).toBeTruthy();
+  const result_2 = checkboxState(7);
+  expect(result_2).toBeFalsy();
+  const result_3 = checkboxState(4);
+  expect(result_3).toBeTruthy();
+  const result_4 = checkboxState(15);
+  expect(result_4).toBeFalsy();
+  const result_5 = checkboxState(1);
+  expect(result_5).toBeTruthy();
+  const result_6 = checkboxState(true);
+  expect(result_6).toBeFalsy();
 });
 
 function setup({
